ce031f25d3b1e64b220290c05a292564eed76ed6
Sinan ARTUN <26043673+sinanartun@users.noreply.github.com>
2022-12-03 20:42:19 +0100

    twitter analytics

---
+++ install_python.sh
@@ -0,0 +1,9 @@
+curl -o ./Python-3.9.15.tgz https://www.python.org/ftp/python/3.9.15/Python-3.9.15.tgz
+tar xvf Python-3.9.15.tgz
+cd Python-*/
+./configure --enable-optimizations
+sudo make altinstall
+
+add to path
+/usr/local/bin
+source .bash_profile
